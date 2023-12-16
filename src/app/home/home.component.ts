ae3ce593d3f3d939a0b7b6c565d4e6f743b9050a
Juan Lagunas <dev.jlagunas@gmail.com>
2023-12-16 09:29:48 +0900

    Ng build ran

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgClass],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  userEmail: string = '';
+  isEmailValid: boolean = true;
 
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log(this.userEmail, 'submitted');
+    } else {
+      this.isEmailValid = false;
+      console.log(this.userEmail, 'not submitted');
+      setTimeout(() => {
+        this.isEmailValid = true;
+      }, 1000);
+    }
+  }
 }
